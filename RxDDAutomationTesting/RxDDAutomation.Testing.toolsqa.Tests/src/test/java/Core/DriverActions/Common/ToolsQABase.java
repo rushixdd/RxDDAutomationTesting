8ad0b5f8b3e46679379ac56695dc6d71f8c9cb9a
Rushikesh Dehankar <realrushi.dehankar@gmail.com>
2024-05-29 22:36:19 +0530

    Framework done. First Test run successfully

---
+++ RxDDAutomationTesting/RxDDAutomation.Testing.toolsqa.Tests/src/test/java/Core/DriverActions/Common/ToolsQABase.java
@@ -0,0 +1,11 @@
+package Core.DriverActions.Common;
+
+public class ToolsQABase  extends Base{
+
+    public ToolsQABase(){
+
+    }
+    public void NavigateToToolsqa(){
+        System.out.println("Navigate to ToolsQA");
+    }
+}
---
+++ RxDDAutomationTesting/RxDDAutomation.Testing.toolsqa.Tests/src/test/java/Features/Test1.feature
@@ -0,0 +1,6 @@
+Feature: Test 1
+  Background:
+    Given Navigate to toolsqa
+
+  Scenario: Test Scenario 1
+    And Click on edit
